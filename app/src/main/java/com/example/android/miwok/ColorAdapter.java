58fc025f06e16ad3e8a3d4657ffcff3b48340114
jativar <jativar@hotmail.com>
2017-07-06 09:55:18 -0500

    Version 1 of Project Miwok. Revision 8 Purpose:  implementation of the ColorAdapter and display the list of colors in Miwok and English (default languaje)

---
+++ app/src/main/java/com/example/android/miwok/ColorAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.miwok;
+
+        import android.app.Activity;
+        import android.support.annotation.NonNull;
+        import android.support.annotation.Nullable;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.TextView;
+
+        import java.util.ArrayList;
+
+/**
+ * Created by Belenchis on 03/07/2017.
+ */
+
+public class ColorAdapter extends ArrayAdapter<Color> {
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+
+        Color currentColor = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentColor.getmMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentColor.getmDefaultTranslation());
+
+        return listItemView;
+
+        //return super.getView(position, convertView, parent);
+
+    }
+
+    public ColorAdapter(Activity context, ArrayList<Color> colors){
+        super(context, 0, colors);
+
+    }
+}
--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.Adapter;
+        import android.widget.ArrayAdapter;
+        import android.widget.GridView;
+        import android.widget.ListView;
+
+        import java.util.ArrayList;
+
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +16,52 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        //TODO: Create an ArrayList colors
+        ArrayList<Color> colors = new ArrayList<Color>();
+
+        //TODO: Add elements in the ArrayList colors
+        //words.add("one");
+        colors.add(new Color("red","wetetti"));
+        colors.add(new Color("green","chokokki"));
+        colors.add(new Color("brown","takaakki"));
+        colors.add(new Color("gray","topoppi"));
+        colors.add(new Color("black","kululli"));
+        colors.add(new Color("white","kelelli"));
+        colors.add(new Color("dusty yellow","topiisә "));
+        colors.add(new Color("mustard yellow","chiwiitә"));
+
+        //TODO: WHEN TO CREATE A CUSTOM CLASS?
+        /* Create the Word class for the Miwok app, so we can show a list
+        of English and Miwok words on the screen for the numbers 1-10.
+        The Word object should contain as data type 2 String variables. One for
+        English Translation, and one for the Miwok translation of the same word.
+        The methods that should have are miwokTranslation "getMiwokTranslation()" and
+        defaultTranslation "getDefaultTranslation()"
+         */
+
+
+        //TODO: Create an {@link Array Adapter}
+        //NB: Spinner, ListView and GridView follow similar concept when used with ArrayAdapter.
+        //Whose data source is a list of String. The
+        //adapter knows how to create layouts for each item in the list, using the
+        //simple_list_item_1.xml layout resource defined in the Android framework.
+        //This list item layout contains a single {@link TextView}, which the adapter will set
+        //to display a single word.
+        ColorAdapter adapter = new ColorAdapter(this, colors);
+
+        //Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        //There should be a {@link ListView} with the view ID called list, which is declared in the
+        //activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        //Make the {@link ListView} use the {@Link ArrayAdapter} we created above, so that the
+        //{@link List View} will display list items for each word in the list of words.
+        //Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        //1 argument, which is the {@link Array Adapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
+
+
+
     }
 }
